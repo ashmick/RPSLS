e8acbbdf471f28f0422a2c29e2bf533589ef6ddd
ashmick <ashley.mcneill2014@gmail.com>
2022-12-16 21:06:05 -0500

    started player class and child class

---
+++ main.py
@@ -0,0 +1,18 @@
+# must disply rules
+# enter name
+# enter amount of players
+# choose to play ai or human
+
+
+# human playing human
+    # human 1 choose options
+    # human 2 chooses options
+    
+    # winner is decided
+    # plays again
+    # best of 3 Wins
+    
+# human playing ai
+
+
+# ai vs ai
---
+++ player.py
@@ -0,0 +1,12 @@
+class Player:
+    
+    def __init__(self, name) -> None:
+        self.name=name
+        self.gesture= ["Rock", "Paper", "Scissor", "Lizard", "Spock"]
+        self.wins=0
+        self.Gesture_chosen=""
+        pass
+    
+    def choose_your_guesture(self):
+        print
+        pass
